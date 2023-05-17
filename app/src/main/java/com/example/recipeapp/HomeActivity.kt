2ca021aa047d0b7af4bd97b753b05014fb2dbb8e
曾荣豪 <2755942193@qq.com>
2023-05-17 14:24:40 +0800

    完成在home_activity展示喜欢列表

--- app/src/main/java/com/example/recipeapp/HomeActivity.kt
+++ app/src/main/java/com/example/recipeapp/HomeActivity.kt
@@ -1,18 +1,30 @@
 package com.example.recipeapp
 
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.recipeapp.Interfaces.GetDataService
+import com.example.recipeapp.adapter.FavouriteAdapter
 import com.example.recipeapp.adapter.MainCategoryAdapter
 import com.example.recipeapp.adapter.SubCategoryAdapter
 import com.example.recipeapp.database.RecipeDatabase
-import com.example.recipeapp.entities.Category
-import com.example.recipeapp.entities.CategoryItems
-import com.example.recipeapp.entities.MealsItems
-import com.example.recipeapp.entities.Recipes
+import com.example.recipeapp.entities.*
+import com.example.recipeapp.retrofitclient.RetrofitClientInstance
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.yield
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 //主界面
 class HomeActivity : BaseActivity() {
@@ -24,6 +36,9 @@ class HomeActivity : BaseActivity() {
     //主次菜单RecyclerView的Adapter
     var mainCategoryAdapter = MainCategoryAdapter()
     var subCategoryAdapter = SubCategoryAdapter()
+    var favCategoryAdapter = FavouriteAdapter()
+
+    var favs : ArrayList<MealsItems> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,11 +46,48 @@ class HomeActivity : BaseActivity() {
 
 
         getDataFromDb()
+        getDataFromProvider()
 
         mainCategoryAdapter.setClickListener(onCLicked)
         subCategoryAdapter.setClickListener(onCLickedSubItem)
+        favCategoryAdapter.setClickListener(onCLickedFavItem)
 
 
+    }
+
+
+    private fun getDataFromProvider() {
+        // 获取所有favourite
+        val fav_cursor = contentResolver.query(Uri.parse("content://com.example.recipeapp.provider/favourite") ,null,null,null,null)
+
+
+        var meals = ArrayList<String>()
+
+
+        fav_cursor?.moveToFirst()
+        var i = 0
+        // 对每一个id调用
+        while(fav_cursor?.isAfterLast != true){
+            if (fav_cursor != null) {
+                meals.add(fav_cursor.getString(0))
+            }
+            i++
+            if (fav_cursor != null) {
+                fav_cursor.moveToNext()
+            }
+        }
+        if (fav_cursor != null) {
+            fav_cursor.close()
+        }
+
+        getSpecificItem(meals)
+
+
+
+        val rv_fav_category = findViewById<RecyclerView>(R.id.rv_fav_category)
+
+        rv_fav_category.layoutManager = LinearLayoutManager(this@HomeActivity,LinearLayoutManager.HORIZONTAL,false)
+        rv_fav_category.adapter = favCategoryAdapter
 
     }
 
@@ -55,9 +107,60 @@ class HomeActivity : BaseActivity() {
         }
     }
 
+    // 函数指针，点击之后执行onClicked函数, 传送到DetailActivity
+    private val onCLickedFavItem  = object : FavouriteAdapter.OnItemClickListener{
+        override fun onClicked(id: String) {
+            var intent = Intent(this@HomeActivity,DetailActivity::class.java)
+            intent.putExtra("id",id)
+            startActivity(intent)
+        }
+    }
 
 
 
+     fun getSpecificItem(meals:ArrayList<String>) {
+        // 获取网络获取数据的MealsEntity
+        val service = RetrofitClientInstance.retrofitInstance!!.create(GetDataService::class.java)
+
+         for(i in meals.indices){
+             val call = service.getSpecificItem(meals[i])
+             call.enqueue(object : Callback<MealResponse> {
+
+                 override fun onFailure(call: Call<MealResponse>, t: Throwable) {
+
+                     Toast.makeText(this@HomeActivity, "Something went wrong" + t.message, Toast.LENGTH_SHORT )
+                         .show()
+                 }
+
+                 override fun onResponse(
+                     call: Call<MealResponse>,
+                     response: Response<MealResponse>
+                 ) {
+                    // 只能这样做了，要想获取图片，网络获取到一个就加入一个
+                     val enti = response.body()!!.mealsEntity[0]
+                        val meal = MealsItems(
+                            favs.size,
+                            enti.idmeal,
+                            enti.strcategory,
+                            enti.strmeal,
+                            enti.strmealthumb
+                        )
+                     favs.add(meal)
+                     favCategoryAdapter.setData(favs)
+                     val rv_fav_category = findViewById<RecyclerView>(R.id.rv_fav_category)
+
+                     rv_fav_category.layoutManager = LinearLayoutManager(this@HomeActivity,LinearLayoutManager.HORIZONTAL,false)
+                     rv_fav_category.adapter = favCategoryAdapter
+                 }
+
+             })
+         }
+
+
+
+
+
+    }
 
 
     private fun getDataFromDb(){
---
+++ app/src/main/java/com/example/recipeapp/adapter/FavouriteAdapter.kt
@@ -0,0 +1,59 @@
+package com.example.recipeapp.adapter
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.recipeapp.R
+import com.example.recipeapp.entities.Recipes
+import android.widget.TextView
+import com.bumptech.glide.Glide
+import com.example.recipeapp.entities.MealsItems
+
+//注释见MainCategoryAdapter，格式类似
+class FavouriteAdapter: RecyclerView.Adapter<FavouriteAdapter.RecipeViewHolder>() {
+
+    var listener: FavouriteAdapter.OnItemClickListener? = null
+    var ctx : Context? = null
+    var arrSubCategory = ArrayList<MealsItems>()
+    class RecipeViewHolder(view: View): RecyclerView.ViewHolder(view){
+
+    }
+
+    fun setData(arrData : List<MealsItems>){
+        arrSubCategory = arrData as ArrayList<MealsItems>
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecipeViewHolder {
+        ctx = parent.context
+        return RecipeViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_rv_fav_category,parent,false))
+    }
+
+    override fun getItemCount(): Int {
+        return arrSubCategory.size
+    }
+
+    fun setClickListener(listener1: FavouriteAdapter.OnItemClickListener){
+        listener = listener1
+    }
+
+    override fun onBindViewHolder(holder: RecipeViewHolder, position: Int) {
+
+        val tvDishName = holder.itemView.findViewById<TextView>(R.id.tv_dish_name2)
+
+        tvDishName.text = arrSubCategory[position].strMeal
+
+        // 正确的类型是AppCompatImageView，不是ImageView
+        val img_dish = holder.itemView.findViewById<androidx.appcompat.widget.AppCompatImageView>(R.id.img_dish2)
+
+        Glide.with(ctx!!).load(arrSubCategory[position].strMealThumb).into(img_dish)
+
+        holder.itemView.rootView.setOnClickListener {
+            listener!!.onClicked(arrSubCategory[position].idMeal)
+        }
+    }
+
+    interface OnItemClickListener{
+        fun onClicked(id:String)
+    }
+}
